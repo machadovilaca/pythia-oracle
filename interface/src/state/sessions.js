a686872bdab1a6555250d756d193f1df112af646
machadovilaca <machadovilaca@gmail.com>
2019-12-18 17:05:18 +0000

    Update state managment

--- interface/src/state/sessions.js
+++ interface/src/state/sessions.js
@@ -1,17 +1,9 @@
-import { getData, updateData } from './genericState';
+import { getData } from './genericState';
 
 export const getCurrentSessions = () => {
   return getData('sessions');
 };
 
-export const updateCurrentSessions = () => {
-  return updateData('sessions');
-};
-
 export const getSessionsHistory = (unit = 'daily', quantity = 7) => {
   return getData('sessions', unit, quantity);
-};
-
-export const updateSessionsHistory = (unit = 'daily', quantity = 7) => {
-  return updateData('sessions', unit, quantity);
 };
--- interface/src/state/users.js
+++ interface/src/state/users.js
@@ -1,17 +1,9 @@
-import { getData, updateData } from './genericState';
+import { getData } from './genericState';
 
 export const getCurrentUsers = () => {
   return getData('users');
 };
 
-export const updateCurrentUsers = () => {
-  return updateData('users');
-};
-
 export const getUsersHistory = (unit = 'daily', quantity = 7) => {
   return getData('users', unit, quantity);
-};
-
-export const updateUsersHistory = (unit = 'daily', quantity = 7) => {
-  return updateData('users', unit, quantity);
 };
